1d5fe4c8e4071aa2f7711f09d207a23dd8b57536
Evgeny <ebmooc@gmail.com>
2021-03-11 22:34:11 +0300

    add form class

--- main.js
+++ main.js
@@ -46,6 +46,14 @@ module.exports = __webpack_require__.p + "static/img/4c0ab963bd09f34cbe97.svg";
 
 /***/ }),
 
+/***/ 207:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "static/img/da5c42223be72e07b420.svg";
+
+/***/ }),
+
 /***/ 378:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
@@ -67,6 +75,7 @@ module.exports = __webpack_require__.p + "static/img/3c70b53795b8b86b5bd4.svg";
 
 var map = {
 	"./add.svg": 782,
+	"./cancel.svg": 207,
 	"./delete.svg": 378,
 	"./edit.svg": 898
 };
@@ -181,7 +190,7 @@ class User {
 }
 
 class Todo {
-  constructor({id, title, description = '', dueDate, priority} = {}) {
+  constructor({id = uniqid_default().time(), title, description = '', dueDate, priority} = {}) {
     this.id = id
     this.title = title
     this.description = description
@@ -212,26 +221,52 @@ class NullUser {
 }
 
 
-;// CONCATENATED MODULE: ./src/view.js
-//import all images in Map ['src path', 'dist path']
-function importAll(r) {
-  return new Map(r.keys().map((value) => [value, r(value)] ));
+;// CONCATENATED MODULE: ./src/utils.js
+class Utils { 
+    static createIcon({url, alt = '', className = ''}) {
+        const myImg = new Image()
+        myImg.src = url
+        myImg.alt = alt
+        //myImg.classList.add(className)
+        return myImg
+        }
+        updateIcon() {
+
+        }
+    // import all images in Map ['src path', 'dist path']
+    static importAll(r) {
+        return new Map(r.keys().map((value) => [value, r(value)] ))
+    }
+
 }
 
-const view_images = importAll(__webpack_require__(606));
+const utils_images = Utils.importAll(__webpack_require__(606))
+
+  
+;// CONCATENATED MODULE: ./src/view.js
+
+
 
 class View {
     constructor() {
       this.todoContainer = document.querySelector('.todos-container')
-      this.addButton = document.querySelector('.todos-container__add-button')
-      this.viewIcon = new ViewIcon()
+      this.addTodoFormButton = document.querySelector('.todos-container__add-button')
+      
+      this.menuContainer = document.querySelector('.menu-container__projects')
+      this.addProjectButton = document.querySelector('.menu-container__add-button')
+      
+      this.buttons = [{button: this.addProjectButton, alt: 'add project', url: utils_images.get('./add.svg'), textContent: 'Add project'},
+      { button: this.addTodoFormButton, alt: 'addTodo', url: utils_images.get('./add.svg'), textContent: 'Add task'}]
     }
-
+    /**
+     * 
+     * @param {*} param0 
+     */
     renderTodo({title}) {
       let li = document.createElement('li'),
-        input = document.createElement('input'),
-        desc = document.createElement('div'),
-        controls = document.createElement('div')
+          input = document.createElement('input'),
+          desc = document.createElement('div'),
+          controls = document.createElement('div')
 
       li.classList.add('todos-container__item')
       input.setAttribute('type', 'checkbox')
@@ -240,27 +275,163 @@ class View {
       desc.classList.add('todos-container__item-title')
       controls.classList.add('todos-container__item-controls')
       
-      controls.append(this.viewIcon.createIcon({url: view_images.get('./edit.svg'), alt: 'edit', className: 'todos-container__item-controls-edit' }))
-      controls.append(this.viewIcon.createIcon({url: view_images.get('./delete.svg'), alt: 'delete', className: 'todos-container__item-controls-delete' }))
+      controls.append(Utils.createIcon({
+        url: utils_images.get('./edit.svg'), 
+        alt: 'edit', 
+        className: 'todos-container__item-controls-edit' 
+      }))
+      controls.append(Utils.createIcon({
+        url: utils_images.get('./delete.svg'), 
+        alt: 'delete', 
+        className: 'todos-container__item-controls-delete' 
+      }))
 
+      
       desc.textContent = title
       li.append(input, desc, controls)
 
       this.todoContainer.append(li)
     }
+  /**
+   * 
+   * @param {*} param0 
+   */
+  renderProject({title}) {
+    let li = document.createElement('li'),
+        span = document.createElement('span')
 
-}
+    li.classList.add('menu-container__subitem')
+    span.textContent = title
 
-class ViewIcon {
-  createIcon({url, alt = '', className = ''}) {
-    const myImg = new Image()
-    myImg.src = url
-    myImg.alt = alt
-    myImg.classList.add(className)
-    return myImg
+    li.append(Utils.createIcon({
+      url: utils_images.get('./delete.svg'), 
+      alt: 'delete'
+    }))
+
+    li.append(span)
+
+    li.append(Utils.createIcon({
+      url: utils_images.get('./delete.svg'), 
+      alt: 'delete'
+    }))
+    //  li.textContent = title
+    this.menuContainer.append(li)
+  }
+  /**
+   * 
+   */
+  renderButtons() {
+    for(let item of this.buttons) {
+      let div = document.createElement('div')  
+      item.button.append(Utils.createIcon({
+        url: item.url, 
+        alt: item.alt
+      }))
+      div.textContent = item.textContent
+      item.button.append(div)
+    }
+  }
+  /**
+   * 
+   */
+  hideAddButton() {
+    this.addTodoFormButton.style.display = 'none'
+  }
+  viewAddButton() {
+    this.addTodoFormButton.style.display = 'flex'
   }
 }
+
 /* harmony default export */ const view = (View);
+;// CONCATENATED MODULE: ./src/viewForm.js
+
+
+class FormHandler {
+    constructor() {
+      this.todoForm = null
+      this.mainContainer = document.querySelector('.main-content')
+      this.menuContainer = document.querySelector('.menu-container__projects')
+      this.closeTodoFormButton = null
+      this.addTodoFormButton = null
+    }
+  
+    fetchTodoForm() {
+  
+    }
+  
+    fetchprojectForm() {
+  
+    }
+  
+    renderTodoForm() {
+      if(this.todoForm) {
+        this.todoForm.style.display = 'flex'
+        return 
+      }
+      let formContainer = document.createElement('div'),
+          buttonsContainer = document.createElement('div'),
+          inputTitle = document.createElement('input'),
+          inputDescription = document.createElement('input'),
+          inputDate = document.createElement('input'),
+          inputPriority = document.createElement('select'),
+          submitButton = document.createElement('button'),
+          options = ['low', 'medium', 'high'];
+          this.closeTodoFormButton = this.initButton({url:utils_images.get('./cancel.svg'),textContent:'Close', alt: 'close form', classList: 'todo-form__close-button'})
+          this.addTodoFormButton = this.initButton({url:utils_images.get('./add.svg'), alt:'add todo', textContent: 'Add', classList: 'todo-form__add-button'})
+          
+          
+          formContainer.classList.add('todo-form')
+          buttonsContainer.classList.add('todo-form__buttons-container')
+          inputTitle.type = 'text'
+          inputTitle.placeholder = 'add title'
+          inputTitle.required = 'true'
+          inputDescription.type = 'text'
+          inputDescription.placeholder ='add description'
+          inputDate.type = 'date'
+          
+          
+          submitButton.append(Utils.createIcon({
+            url: utils_images.get('./add.svg'), 
+            alt: 'addTodo'
+          }))
+          
+          for(let item of options) {
+            let option = document.createElement('option')
+            option.textContent = item
+            inputPriority.append(option)
+          }
+          
+          buttonsContainer.append(this.addTodoFormButton, this.closeTodoFormButton)
+          formContainer.append(inputTitle, inputDescription, inputDate, inputPriority, buttonsContainer)
+          this.todoForm = formContainer
+          this.mainContainer.append(formContainer)
+    }
+  
+    initButton({url, alt, textContent, classList}) { 
+      let closeButton = document.createElement('div'),
+          textElem = document.createElement('div')
+          textElem.textContent = textContent
+      closeButton.classList.add(classList)
+      closeButton.append(Utils.createIcon({
+        url: url, 
+        alt: alt
+      }), textElem)
+
+      return closeButton
+    }
+
+
+  
+    hideForm() {
+      this.todoForm.style.display = 'none'
+    }
+  
+    renderProjectForm() {
+  
+    }
+  }
+
+/* harmony default export */ const viewForm = (FormHandler);
 ;// CONCATENATED MODULE: ./src/eventEmitter.js
 const eventEmitter = (function event() {
     let events = {};
@@ -301,6 +472,7 @@ const eventEmitter = (function event() {
 
 
 
+
 class Controller {
   
 }
@@ -311,19 +483,46 @@ class EventController {
         this.view = view
         this.project = new Project()  
         this.todo = {}
+        this.formHandler = new viewForm()
 
-        this.render = (data) => this.view.renderTodo(data)
+        this.renderTodo = (data) => this.view.renderTodo(data)
+        this.renderProject = (data) => this.view.renderProject(data)
+
         this.addTodoToModel = (data) => this.project.addTodo(data)
-    }
+        // db func this.addProjectToModel = (data) => 
+      }
 
     init() {
+      document.addEventListener('DOMContentLoaded', (e) => {
+        this.view.renderButtons()
+      })
+
+      
+      this.evt.on('addTodoForm', () => this.formHandler.closeTodoFormButton.addEventListener('click', (e) => {   
+        this.view.viewAddButton()
+        this.formHandler.hideForm()
+      }))
+
+      this.evt.on('addTodoForm', () => this.formHandler.addTodoFormButton.addEventListener('click', () => {
+        this.evt.emit('addTodo', {title: 'added'});
+      }))
       this.evt.on('addTodo', this.addTodoToModel)
-      this.evt.on('addTodo', this.render)
-      this.evt.on('addTodo', ()=>console.log(this.project))
+      this.evt.on('addTodo', this.renderTodo)
+      this.evt.on('addTodo', () =>console.log(this.project))
 
+      // db func this.evt.on('addProject', this.addProjectToModel)
+      this.evt.on('addProject', this.renderProject)
 
-      this.view.addButton.addEventListener('click', (e) => {
-        this.evt.emit('addTodo', {id: 1, title: 'added'})
+      this.evt.emit('addTodo', {title: 'first'})
+
+      this.view.addTodoFormButton.addEventListener('click', (e) => {
+        this.view.hideAddButton()
+        this.formHandler.renderTodoForm()
+        this.evt.emit('addTodoForm', '')//{id: 1, title: 'added'})
+      })
+      
+      this.view.addProjectButton.addEventListener('click', (e) => {
+        this.evt.emit('addProject', {title: 'sport'})
       })
     }
 }
